850c5a6e56afee81ffc0ffacd3290faf4c7a009b
Shreyas Subramanya <ssubramanya@ten-x.com>
2018-01-09 15:23:41 -0800

    Fix launching container in local mode

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # slider-spark
 Docker image and utility scripts to launch a dockerized spark cluster through apache slider on a HDP 2.6 yarn cluster. The docker container has R installed and can run sparkR.
 
-## Prerequisites
+## Prerequisites to run in yarn cluster mode with slider
 - /user/slider exists on HDFS and is writable by the user running the slider job
 - run.sh script which would issue spark submit (slider/example folder has a sample)
 - HDP 2.6 cluster with docker installed on the datanodes (modify the bootstrap.sh script for a different hadoop distribution/version)
 - apache slider installed
 
-## Usage
+## Usage in local mode
+```
+Launch container using:
+docker run -d -e CORES=4 -e MEM=4096 -p 8080:8080 shreyass123/slider-spark
+```
+
+## Usage in cluster mode
 ```
 slider/launch.sh <app-name> <folder containing run.sh and spark app dependencies>
 Ex:
--- image/bootstrap.sh
+++ image/bootstrap.sh
@@ -13,18 +13,20 @@ mkdir -p /var/log/spark
 chown -R spark:hadoop /opt/apache/spark* /etc/spark /var/run/spark /var/log/spark
 
 # TODO: use /grid/1 disk for /data?
-# TODO: figure out a better logic to retrieve spark master
-useradd -u 205 -g 123 $SLIDER_APP_USER
 if [[ -e /usr/hdp/current/slider-client/bin/slider ]]; then
+    useradd -u 205 -g 123 $SLIDER_APP_USER
+    # TODO: figure out a better logic to retrieve spark master?
     SPARK_MASTER=$(sudo -u $SLIDER_APP_USER /usr/hdp/current/slider-client/bin/slider status ${SLIDER_APP_NAME} | \
                    sed -n '/\"MASTER\" :/,/}/p' |grep \"host\" | awk -F\" '{print $4}')
+    METASTORE_URI=$(grep "thrift://" /etc/hive/conf/hive-site.xml | sed -n 's:<value>\(.*\)</value>:\1:p' | tr -d '[:space:]')
+    SPARK_METASTORE_OPT="\"-Dhive.metastore.uris=${METASTORE_URI}\""
 else
     # local docker container
     SPARK_MASTER=$(hostname)
+    SPARK_METASTORE_OPT=""
 fi
 SPARK_WORKER_LOCAL=$(hostname)
-METASTORE_URI=$(grep "thrift://" /etc/hive/conf/hive-site.xml | sed -n 's:<value>\(.*\)</value>:\1:p' | tr -d '[:space:]')
-SPARK_METASTORE_OPT="\"-Dhive.metastore.uris=${METASTORE_URI}\""
+
 sed -i "s/^SPARK_WORKER_MEMORY.*/SPARK_WORKER_MEMORY=${MEM}m/g; \
         s/^SPARK_WORKER_CORES.*/SPARK_WORKER_CORES=${CORES}/g; \
         s@^SPARK_MASTER_IP.*@SPARK_MASTER_IP=${SPARK_MASTER}@g" /etc/spark/conf/spark-env.sh
@@ -33,9 +35,6 @@ sed -i "s/^spark.default.parallelism.*/spark.default.parallelism=${CORES}/g; \
         s@^spark.master.*@spark.master=spark://${SPARK_MASTER}:7077@g; \
         s@spark.driver.extraJavaOptions.*@spark.driver.extraJavaOptions=${SPARK_METASTORE_OPT}@g" /etc/spark/conf/spark-defaults.conf
 
-sed -i "s@^SPARK_MASTER@spark://${SPARK_MASTER}:7077@g" /etc/spark/conf/spark-defaults.conf
-sed -i "" /etc/spark/conf/spark-env.sh
-
 # TODO: set non default ports for spark?
 if [[ "${SPARK_MASTER}" == "${SPARK_WORKER_LOCAL}" ]]; then
     #start spark master as well
@@ -43,11 +42,14 @@ if [[ "${SPARK_MASTER}" == "${SPARK_WORKER_LOCAL}" ]]; then
 fi
 sudo -u spark -E /opt/apache/spark/sbin/start-slave.sh spark://$SPARK_MASTER:7077
 
-# Run the app
-if [[ "${SPARK_MASTER}" == "${SPARK_WORKER_LOCAL}" ]]; then
-    sudo -u $SLIDER_APP_USER -E $SPARK_HOME/bin/hadoop fs -get /user/slider/$SLIDER_APP_NAME /tmp
-    sudo -u $SLIDER_APP_USER -E chmod +x /tmp/$SLIDER_APP_NAME/run.sh
-    sudo -u $SLIDER_APP_USER -E /tmp/$SLIDER_APP_NAME/run.sh &
+
+# Run the app if running on the cluster and we are the master
+if [[ -e /usr/hdp/current/slider-client/bin/slider ]]; then
+    if [[ "${SPARK_MASTER}" == "${SPARK_WORKER_LOCAL}" ]]; then
+        sudo -u $SLIDER_APP_USER -E $SPARK_HOME/bin/hadoop fs -get /user/slider/$SLIDER_APP_NAME /tmp
+        sudo -u $SLIDER_APP_USER -E chmod +x /tmp/$SLIDER_APP_NAME/run.sh
+        sudo -u $SLIDER_APP_USER -E /tmp/$SLIDER_APP_NAME/run.sh &
+    fi
 fi
 
 # Leave the container running
